8fa3cae9859f8103b15f3607fbfa6ca019f3e589
Paweł <pawel.bednarz2@gmail.com>
2020-11-20 22:01:02 +0100

    Remove echo from access_control.php

--- controllers/security/access_control.php
+++ controllers/security/access_control.php
@@ -3,7 +3,7 @@
 session_start();
 
 $role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
-echo $role;
+
 // check if any role exist (then user is logged in)
 if (empty($role)) {
     // if there is no role set - render login page
