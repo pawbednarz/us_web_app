9b9a804a29e5e4cd35dda733f89488f5474cf694
Paweł <pawel.bednarz2@gmail.com>
2020-12-12 23:51:14 +0100

    change of server_name and server_url variables

--- classes/Config.class.php
+++ classes/Config.class.php
@@ -2,8 +2,8 @@
 
 class Config{
 
-    public $server_name = "192.168.8.165";
-    public $app_root = "/us_web_app/";
+    public $server_name = "192.168.0.108";
+    public $app_root = "/us_web_app";
     public $root_path = "/mnt/hdd/projects/us_web_app";
 
     public $server_url;
@@ -15,7 +15,7 @@ class Config{
     public $controllers_dir;
 
     public function __construct() {
-        $this->server_url = "https://" . $this->server_name;
+        $this->server_url = "http://" . $this->server_name;
         $this->app_url = $this->server_url . $this->app_root;
         $this->controllers_url = $this->app_url . "/controllers/";
         $this->controllers_dir = $this->root_path . "/controllers/";
